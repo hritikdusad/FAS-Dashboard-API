b32ac77c4f3c6be43f0a2f93f222f1d2d46898d0
Hritikdusad <16ucs078@lnmiit.ac.in>
2020-04-26 20:26:16 +0530

    Adding Sign Off Details

---
+++ Models/SignOffDetails.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Models
+{
+    public class SignOffDetails{
+        public int FundId {get; set;}
+        public string FundName {get; set;}
+        public DateTime ActualDate {get; set;}
+        public DateTime IdealDate {get; set;}
+
+    }
+}
